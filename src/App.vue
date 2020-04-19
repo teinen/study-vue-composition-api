24cba1d3641e6dba4840304ef486d3545397cbee
Akira Kanno <teinen.github@gmail.com>
2020-04-19 14:14:09 +0900

    Remove initial components

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,13 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    sample
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
 
-export default Vue.extend({
-  name: "App",
-  components: {
-    HelloWorld
-  }
-});
+export default Vue.extend({});
 </script>
 
 <style>
